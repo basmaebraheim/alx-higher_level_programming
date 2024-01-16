78117e8531066a426124016acd461333b89d950b
basma ebraheim <basmaebraheim@gmail.com>
2024-01-16 23:08:12 +0200

    6 use string format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i == 8 and j == 9:
-            print(f"{i:d}{j:d}")
+            print("{:d}{:d}".format(i, j))
         elif i < j:
-            print(f"{i:d}{j:d}", end=', ')
+            print("{:d}{:d}".format(i, j), end=', ')
