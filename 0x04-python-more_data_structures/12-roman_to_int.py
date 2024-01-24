e8949738f4425c181044ef97115c8f91c3a1477e
basma ebraheim <basmaebraheim@gmail.com>
2024-01-25 00:44:28 +0200

    update

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ints = [dictionary[x] for x in roman_string]
+    result = 0
+    for i in range(len(ints)):
+        result += ints[i]
+        if ints[i - 1] < ints[i] and i != 0:
+            result -= (ints[i - 1] + ints[i - 1])
+    return result
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    result = 0
+    for i in list(a_dictionary.keys()):
+        result += 1
+    return result
