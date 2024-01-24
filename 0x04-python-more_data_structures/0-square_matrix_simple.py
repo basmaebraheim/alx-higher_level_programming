fbbf7308a6dd442a3bd47272c4a3da1caf6fe1ff
basma ebraheim <basmaebraheim@gmail.com>
2024-01-24 23:34:21 +0200

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return None
-    return list(map(lambda arr: (list(map(lambda x : x**2,arr))), matrix))
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
