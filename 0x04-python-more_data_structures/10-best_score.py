b4819763c93a2dab201a72ff0dfd445fd4dfb67f
basma ebraheim <basmaebraheim@gmail.com>
2024-01-25 10:15:45 +0200

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary or len(a_dictionary) == 0:
         return None
-    
+
     max = list(a_dictionary.keys())[0]
     for key in a_dictionary:
         if a_dictionary[key] > a_dictionary[max]:
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,15 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dictionary = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
     ints = [dictionary[x] for x in roman_string]
     result = 0
     for i in range(len(ints)):
