fe73a2d3d8ffcddc0e0d661ba7b4945de855c926
basma ebraheim <basmaebraheim@gmail.com>
2024-02-05 22:09:42 +0200

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for item in my_list:
+    for m in range(x):
         try:
-            if x>i:
-                print("{}".format(item),end="")
-                i += 1
+            print(my_list[m], end="")
+            i += 1
         except IndexError:
             break
     print()
