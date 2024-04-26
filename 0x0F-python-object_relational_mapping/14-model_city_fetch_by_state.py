1b341d92aae82a185ddb05de5e9e14f404f716dc
basma ebraheim <basmaebraheim@gmail.com>
2024-04-26 23:18:16 +0300

    filter states using ORM

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""prints all City objects from the database hbtn_0e_14_usa"""
-
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from model_state import Base, State
 from model_city import City
