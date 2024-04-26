dfd9e70381ced56486b71d40fba6200862d7ae6a
basma ebraheim <basmaebraheim@gmail.com>
2024-04-26 22:58:18 +0300

    filter states using ORM

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """get city with state"""
+
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
