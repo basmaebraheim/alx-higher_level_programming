d0763dae2426146ac59c295fe0b5d7e1f3e2787e
basma ebraheim <basmaebraheim@gmail.com>
2024-04-26 21:11:12 +0300

    start use orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""  takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa """
 import MySQLdb
 import sys
 
@@ -8,7 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4],))
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states. LIKE %s""", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
